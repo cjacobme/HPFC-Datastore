2ae9952d14f7ec66a74698112be4e4b9bed725a0
cjacobme <cjacobme@aol.com>
2017-09-30 22:16:48 +0200

    added media type

--- CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/simple/service/SimpleRestService.java
+++ CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/simple/service/SimpleRestService.java
@@ -2,6 +2,8 @@ package cj.software.hpfc.simple.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import cj.software.hpfc.simple.model.KeyValuePair;
 
@@ -17,6 +19,7 @@ public class SimpleRestService
 
 	@GET
 	@Path("/json")
+	@Produces(MediaType.APPLICATION_JSON)
 	public KeyValuePair json()
 	{
 		KeyValuePair lResult = new KeyValuePair("Key #1", "This is the value for Key #1");
