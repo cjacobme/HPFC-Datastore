92b2d669658787bca7aeed8f8952fea5d94ae4f8
cjacobme <cjacobme@aol.com>
2017-10-02 18:40:38 +0200

    Mit einem Accessor arbeiten

--- CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/weather/imports/dao/WeatherImportAccessor.java
+++ CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/weather/imports/dao/WeatherImportAccessor.java
@@ -1,7 +1,10 @@
 package cj.software.hpfc.weather.imports.dao;
 
+import java.util.List;
+
 import com.datastax.driver.mapping.Result;
 import com.datastax.driver.mapping.annotations.Accessor;
+import com.datastax.driver.mapping.annotations.Param;
 import com.datastax.driver.mapping.annotations.Query;
 
 import cj.software.hpfc.weather.imports.entity.ImportDirectory;
@@ -9,6 +12,6 @@ import cj.software.hpfc.weather.imports.entity.ImportDirectory;
 @Accessor
 public interface WeatherImportAccessor
 {
-	@Query("select * from directory_finished where bucket=1 and directory_name in (?, ?)")
-	public Result<ImportDirectory> listDirectories(String... pDirNames);
+	@Query("select * from directory_finished where bucket=1 and directory_name in :directories")
+	public Result<ImportDirectory> listDirectories(@Param("directories") List<String> pInClause);
 }
--- CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/weather/imports/dao/WeatherImportDAO.java
+++ CJ  -  HPFC Datastore/src/main/java/cj/software/hpfc/weather/imports/dao/WeatherImportDAO.java
@@ -2,14 +2,15 @@ package cj.software.hpfc.weather.imports.dao;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
 
 import cj.software.hpfc.weather.imports.entity.ImportDirectory;
 
@@ -23,30 +24,18 @@ public class WeatherImportDAO implements Serializable
 
 	public List<ImportDirectory> listDirectories(String... pSearched)
 	{
-		String lInClause = this.toInClause(pSearched);
-		String lQL = "SELECT * FROM directory_finished WHERE bucket=1 AND directory_name in (" + lInClause + ")";
-		ResultSet lRead = this.session.execute(lQL);
-		List<Row> lRows = lRead.all();
-		List<ImportDirectory> lResult = new ArrayList<>();
-		for (Row bRow : lRows)
-		{
-			String lDirName = bRow.getString("directory_name");
-			boolean lFinished = bRow.getBool("directory_finished");
-			ImportDirectory lImportDirectory = new ImportDirectory(lDirName, lFinished);
-			lResult.add(lImportDirectory);
-		}
+		MappingManager lMappingManager = new MappingManager(this.session);
+		WeatherImportAccessor lAccessor = lMappingManager.createAccessor(WeatherImportAccessor.class);
+		List<String> lAsList = this.toStringList(pSearched);
+		Result<ImportDirectory> lRead = lAccessor.listDirectories(lAsList);
+		List<ImportDirectory> lResult = lRead.all();
 		return lResult;
 	}
 
-	private String toInClause(String... pItems)
+	private List<String> toStringList(String... pItems)
 	{
-		StringBuilder lSB = new StringBuilder();
-		for (String bItem : pItems)
-		{
-			lSB.append("'").append(bItem).append("', ");
-		}
-		int lLength = lSB.length();
-		lSB.delete(lLength - 2, lLength);
-		return lSB.toString();
+		List<String> lResult = new ArrayList<>(pItems.length);
+		lResult = Arrays.asList(pItems);
+		return lResult;
 	}
 }
